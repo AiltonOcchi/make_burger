11a12ff8a6ee0ec5354395604842f6456166f011
Ailton Occhi <ailton.occhi@hotmail.com>
2022-06-16 15:18:34 -0300

    Criando o Banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte o seu burger:</h1>
+    <p>formulario</p>
   </div>
 </template>
+
+<script>
+
+import Banner from '../components/Banner.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Banner
+  }
+}
+</script>
